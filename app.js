dcfb00e845156feba169decafaad3b8c8b19fcd1
OmarAbousamra30 <omarmahmoudabousamra@gmail.com>
2025-07-23 16:52:24 +0300

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    
+    header.classList.add('hide');
+  } else {
+   
+    header.classList.remove('hide');
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+});
